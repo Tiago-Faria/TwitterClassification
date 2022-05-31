52856097d48dd075d415d248a33642f13fb336c8
Marcio Antonio <marcio55afr@gmail.com>
2022-05-31 16:24:17 -0300

    Get home timeline with Essencial access

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,4 @@
 from src.utils.tweets_twitter import get_subject
-from src.utils.tweets_tweepy import get_timeline
+from src.utils.tweets_tweepy import get_home_timeline
 
-__all__ = [get_subject, get_timeline]
+__all__ = [get_subject, get_home_timeline]
