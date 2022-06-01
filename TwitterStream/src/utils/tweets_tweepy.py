2b40fc9575ef3d29913f09a6a210bc93abfb117b
Marcio Antonio <marcio55afr@gmail.com>
2022-06-01 14:01:50 -0300

    Fixing get home time line function

--- TwitterStream/src/utils/tweets_tweepy.py
+++ TwitterStream/src/utils/tweets_tweepy.py
@@ -1,10 +1,8 @@
 import configparser
 import tweepy
-from tweepy.asynchronous import AsyncClient
-import asyncio
 
 config = configparser.ConfigParser()
-print(config.read('src/config.ini'))
+config.read('TwitterStream/src/config.ini') 
 
 bearer_token = config['your.keys']['bearer_token']
 api_key = config['your.keys']['api_key']
@@ -12,11 +10,16 @@ api_key_secret = config['your.keys']['api_key_secret']
 access_token = config['your.keys']['access_token']
 access_token_secret = config['your.keys']['access_token_secret']
 
-auth = tweepy.OAuthHandler(api_key, api_key_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth)
 client = tweepy.Client(bearer_token, api_key, api_key_secret, access_token, access_token_secret)
 
-def get_home_timeline():
-    print(client.get_home_timeline(max_results=20))
+
+def get_home_timeline(max_results:int) -> tweepy.Response:
+    """Get tweets from the timeline associated with the api_key
+
+    Args:
+        max_results (int): maximum number of tweets to return up to a maximum of 100
+
+    Returns:
+        tweepy.Response: tweepy object containing the tweets and some meta data
+    """
+    return client.get_home_timeline(max_results=max_results)
